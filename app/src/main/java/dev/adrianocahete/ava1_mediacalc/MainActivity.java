f7d309df940df6a9a4cc25d15c7f18497c34a594
Adriano Cahete <me@adrianocahete.dev>
2025-09-08 11:39:50 -0300

    feat(app): fix icons + remove unused code + update packages + add header + add navigation

--- app/src/main/java/dev/adrianocahete/ava1_mediacalc/MainActivity.java
+++ app/src/main/java/dev/adrianocahete/ava1_mediacalc/MainActivity.java
@@ -1,58 +1,88 @@
 package dev.adrianocahete.ava1_mediacalc;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 import dev.adrianocahete.ava1_mediacalc.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view ->
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null)
-                        .setAnchorView(R.id.fab).show();
-            }
-        });
-        DrawerLayout drawer = binding.drawerLayout;
-        NavigationView navigationView = binding.navView;
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+                        .setAnchorView(R.id.fab).show()
+        );
+
+        DrawerLayout drawer = binding.getRoot();
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        setupNavigationHeader(navigationView);
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_courses, R.id.nav_slideshow)
+                R.id.nav_home, R.id.nav_courses)
                 .setOpenableLayout(drawer)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment_content_main);
+        NavController navController = navHostFragment.getNavController();
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
     }
 
+    private void setupNavigationHeader(NavigationView navigationView) {
+        View headerView = navigationView.getHeaderView(0);
+        TextView navHeader = headerView.findViewById(R.id.navHeader);
+        TextView navAccount = headerView.findViewById(R.id.navAccount);
+
+        AccountManager accountManager = AccountManager.get(this);
+        Account[] accounts = accountManager.getAccountsByType("com.google");
+
+        if (accounts.length > 0) {
+            Account account = accounts[0];
+            String email = account.name;
+
+            String user = email.contains("@") ? email.substring(0, email.indexOf("@")) : email;
+
+            navHeader.setText(user);
+            navAccount.setText(email);
+        } else {
+            navHeader.setText("User");
+            navAccount.setText("");
+        }
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
